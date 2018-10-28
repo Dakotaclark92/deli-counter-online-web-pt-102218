ced5db67ac6b69e44183fba27db8b2824fb1faa4
Dakota Clark <bannaray95@gmail.com>
2018-10-28 04:19:36 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,16 +1,41 @@
-def line(katz_deli)
-  line_array = []
-  count = 0
+var katzDeli = [];
+
+function currentLine(line){
+  if(!line.length) {
+    return "The line is currently empty.";
+  }
+  var lineNamesandNumbers = [];
   
-  if katz_deli.length == 0
-    return "The line is currently empty."
-  else
-    katz_deli.each do |name|
-      
-      line_array << ("#{count += 1}. #{name}")
-      
-    end
-  end
+  for(var i=0; i<line.length; i++) {
+    lineNamesandNumbers.push(i+1 + ". "+ line[i]);
+  }
+  console.log("The line is currently: " + lineNamesandNumbers)
+  return "The line is currently: " + lineNamesandNumbers.join(', ');
+}
+
+function nowServing(line) {
+  if(!line.length) {
+    console.log("There is nobody waiting to be served!")
+    return "There is nobody waiting to be served!"
+  } else {
+    //console.log("Currently serving " + line.shift());
+    return "Currently serving " + line.shift();
+  }
+}
+
+function takeANumber(line, name){
+  line.push(name);
   
-  return "The line is currently: #{line_array.join(" ")}"
-end
+  console.log("Welcome, " + name + ". You are number " + line.length + " in line.");
+  
+  return "Welcome, " + name + ". You are number " + line.length + " in line."
+}
+takeANumber(katzDeli, "Ada")
+takeANumber(katzDeli, "Grace")
+takeANumber(katzDeli, "Kent")
+currentLine(katzDeli);
+nowServing(katzDeli);
+takeANumber(katzDeli, "Matz"); 
+currentLine(katzDeli); 
+nowServing(katzDeli);
+currentLine(katzDeli)
